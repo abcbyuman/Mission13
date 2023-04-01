3087a53941c2b8d293b94077b38b48a70bb44a9a
abcbyuman <95644079+abcbyuman@users.noreply.github.com>
2023-03-31 23:50:29 -0600

    Mission13 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-
-function GoButton(props: any) {
-  return (
-    <div>
-      <button onClick={props.blahClick}>Go!</button>
-      <br />
-    </div>
-  );
-}
-
-function AmountOfGo(props: any) {
-  return <label>{props.num}</label>;
-}
+import React from 'react';
+import TopBanner from './Blah';
+import MovieList from './Movies';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <GoButton blahClick={() => setCount(count + 1)} />
-      <AmountOfGo num={count} />
+    <div className="App">
+      <TopBanner saying="Austins React Website - Main" />
+      <MovieList />
     </div>
   );
 }
---
+++ src/Movies.tsx
@@ -0,0 +1,59 @@
+import data from './MovieDataSample.json';
+import { useState } from 'react';
+
+const mds = data.MovieDataSampleList;
+
+function MovieList() {
+  const [listOMovies, setListOMovies] = useState(mds);
+
+  const addMovie = () => {};
+  setListOMovies([
+    ...mds,
+    {
+      MovieId: 6,
+      Category: 'Action/Adventure',
+      Title: 'Batman Returns',
+      Year: 1992,
+      Director: 'Tim Burton',
+      Rating: 'PG-13',
+    },
+  ]);
+  return (
+    <>
+      <div>
+        <h1>Joel Hilton's Movie Collection</h1>
+      </div>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Year</th>
+              <th>Director</th>
+              <th>Rating</th>
+              <th>Caategory</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mds.map((m) => (
+              <tr key={m.MovieId}>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <td>{m.Director}</td>
+                <td>{m.Rating}</td>
+                <td>{m.Category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <button className="btn btn=primary" onClick={addMovie}>
+        Add Movie
+      </button>
+    </>
+  );
+}
+
+export default MovieList;
